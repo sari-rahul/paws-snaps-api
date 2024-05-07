fd3953b70d4e26fd6b07dbd6ca4a71a10542a02a
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-05-07 08:23:30 +0000

    Change profile views to generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,36 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+#from django.db.models import Count
+from rest_framework import generics, permissions
+#from django_filters.rest_framework import DjangoFilterBackend
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Internal:
+from paws_and_snaps_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-class ProfileList(APIView):
+
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve an profile and edit or delete it if you own it.
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+
+
+
+
+    
